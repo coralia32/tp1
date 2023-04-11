2e3eacd69ba6e413e8fd0ec2ec2bc85a57abca55
David Lelievre <davidoule76@hotmail.fr>
2023-04-11 08:51:30 +0200

    version 1.0 de Crypto.java

---
+++ src/Cryptomonaie.java
@@ -0,0 +1,11 @@
+public class Cryptomonaie
+{
+	private String nom;
+	private double valeurDeJeton; //en euro
+	
+	public Criptomonaie (String nom, double valeurDeJeton)
+	{
+		this.nom = nom ;
+		this.valeurDeJeton = valeurDeJeton ;
+	}
+}
